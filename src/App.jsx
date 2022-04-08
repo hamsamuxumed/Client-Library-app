d40d0860bd8b83069dbeaf2fc8d224eac33ccb39
pujatB <76119090+pujatB@users.noreply.github.com>
2022-04-08 13:01:01 +0100

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import {NavBar} from './components';
+
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
